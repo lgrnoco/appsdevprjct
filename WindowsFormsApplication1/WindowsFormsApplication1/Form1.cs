8cd6a371c732030a28f2323e0a60554cd4551a4d
lgrnoco <126637541+lgrnoco@users.noreply.github.com>
2023-03-22 13:14:32 +0800

    Update Form1.cs

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,88 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+    
+     private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = " ";
+            //Checdkbox1 = Japan
+            if (checkBox1.Checked == true)
+            {
+                str = str + checkBox1.Text;
+                str += "";
+            }
+            //Checkbox2 = South Korea
+            if (checkBox2.Checked == true)
+            {
+                str = str + checkBox2.Text;
+                str += "";
+            }
+            //Checkbox3 = Italy
+            if (checkBox3.Checked == true)
+            {
+                str = str + checkBox3.Text;
+                str += "";
+            }
+            //Checkbox4 = Germany
+            if (checkBox4.Checked == true)
+            {
+                str = str + checkBox4.Text;
+                str += "";
+            }
+            if (str != null)
+            {
+                MessageBox.Show("You Selected  \n" + str + "\nThank You!", "Survey", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            othersLabel.Visible = true;
+            othersTextBox.Visible = true;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+
     }
 }
